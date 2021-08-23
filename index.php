0bef093032c513bb0fc706cc71b2a852324a8aa2
Franckligonniere <84440512+Franckligonniere@users.noreply.github.com>
2021-08-23 16:27:45 +0200

    ajout de la video

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require "./components/header.php"; ?>
+
+    <section class="showcase">
+        <h1>bonjour</h1>
+        <video src="./images/ff7.mp4" muted loop autoplay></video>
+        <div class="overlay"></div>
+        
+    </section>
+
+<?php require "./components/footer.php"; ?>
